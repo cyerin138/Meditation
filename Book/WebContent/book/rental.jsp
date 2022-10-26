f70dfa83725ee49805fd32861e9635b4e8832b9c
silsu876 <ys2110106@y-y.hs.kr>
2022-10-26 14:16:14 +0900

    예시 추가

---
+++ Book/WebContent/book/rental.jsp
@@ -0,0 +1,46 @@
+<%@page import="vo.RentVO"%>
+<%@page import="java.util.ArrayList"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="../menu.jsp"%>
+
+<%
+	ArrayList<RentVO> list = (ArrayList<RentVO>) request.getAttribute("rent_list");
+%>
+<h1>도서 대여 결과 화면</h1>
+<table border="1" align="center" width="800px">
+	<tr>
+		<th>사용자ID</th>
+		<th>대출자</th>
+		<th>책이름</th>
+		<th>반납하기</th>
+	</tr>
+	<%
+		if (list != null) {
+			for (RentVO data : list) {
+	%>
+	<tr>
+		<td><%=data.getMemberId()%></td>
+		<td><%=data.getMemberName()%></td>
+		<td><%=data.getBookTitle()%></td>
+
+
+		<%
+			if (login == null) {
+						out.println("<td></td>");
+					} else {
+		%>
+		<td><a href="/returnBook?bookNo=<%=data.getBookNo()%>">반납하기</a></td>
+
+		<%
+			}
+		%>
+	</tr>
+	<%
+		}
+		}
+	%>
+</table>
+
+
+<%@ include file="../footer.jsp"%>
---
+++ Book/src/biz/ReturnBookServlet.java
@@ -0,0 +1,67 @@
+package biz;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BookDAO;
+import dao.RentDAO;
+
+/**
+ * Servlet implementation class ReturnBookServlet
+ */
+@WebServlet("/returnBook")
+public class ReturnBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReturnBookServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		int bookNo;
+		String strBookNo = request.getParameter("bookNo");
+		if(strBookNo == null) {
+			bookNo = 0;
+		} else {
+			bookNo = Integer.parseInt(strBookNo);
+			
+		}
+		RentDAO rentDao = new RentDAO();
+		BookDAO bookDao;
+		int ret = rentDao.deleteRent(bookNo);
+		System.out.println(strBookNo);
+		if( ret > 0) {
+			bookDao = new BookDAO();
+			bookDao.setRentalField(bookNo, "f");
+			
+		} else {
+			out.println("<script> alert('도서 반납 실패'); history.back() ; </script>");
+		}
+		response.sendRedirect("/rentalBook");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
