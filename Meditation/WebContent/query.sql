13b2d616e9cfa915e74f56e7b42c7ba8e8a19d65
silsu876 <87000648+silsu876@users.noreply.github.com>
2022-11-09 01:08:15 +0900

    예비용
    진짜 오늘은 html , css 끝내야 한다 제발요

--- Meditation/WebContent/query.sql
+++ Meditation/WebContent/query.sql
@@ -1,4 +1,26 @@
+--계정 테이블
 create table member(
+	id varchar2(10) constraint member_id_pk primary key,
+	pwd varchar2(10),
+	name varchar2(10)
+);
+
+--영상 테이블
+create table video(
+	id varchar2(10) constraint member_id_pk primary key,
+	pwd varchar2(10),
+	name varchar2(10)
+);
+
+--카테고리 테이블
+create table video(
+	id varchar2(10) constraint member_id_pk primary key,
+	pwd varchar2(10),
+	name varchar2(10)
+);
+
+--댓글 테이블
+create table comment(
 	id varchar2(10) constraint member_id_pk primary key,
 	pwd varchar2(10),
 	name varchar2(10)
