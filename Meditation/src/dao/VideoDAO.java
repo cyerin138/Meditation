0a880e6b11dfc9d35034914eabbd444d0d57fd0d
silsu876 <87000648+silsu876@users.noreply.github.com>
2022-11-09 01:00:17 +0900

    예비용

---
+++ Meditation/src/dao/VideoDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class VideoDAO {
+
+	public VideoDAO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	Connection con = null;
+	PreparedStatement pstmt   = null;
+	ResultSet rs = null;
+
+}
